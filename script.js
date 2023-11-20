59f10318f773f07921ececdcd0341bbded61d452
FRUITYSPIKE <145369924+FRUITYSPIKE@users.noreply.github.com>
2023-11-20 10:16:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.getElementById("idi1").addEventListener("click", function1);
+document.getElementById("idi2").addEventListener("click", function2);
+document.getElementById("idi3").addEventListener("click", function3);
+document.getElementById("idi4").addEventListener("click", function4);
+document.getElementById("idi5").addEventListener("click", function5);
+
+//event functions
+function function1() {
+  alert("rocket science");
+}
+function function2() {
+  alert("the hook");
+}
+
+function function3() {
+  alert("boat");
+}
+
+function function4() {
+  alert("the devil");
+}
+
+function function5() {
+  alert("both worlds");
+}
